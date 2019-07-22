bcf6c53490de3206ee984aa539cda86f521495ad
Evgeny Yakimov <john.yakimov@gmail.com>
2019-07-22 23:08:27 +0100

    Modernised JS in brigade.js

--- brigade.js
+++ brigade.js
@@ -4,32 +4,31 @@ const { events, Job } = require("brigadier");
 //
 // Note that a GitHub App will automatically generate these events
 // from a `push` event, so we don't need an explicit push event handler any longer
-events.on("check_suite:requested", checkRequested);
-events.on("check_suite:rerequested", checkRequested);
-events.on("check_run:rerequested", checkRequested);
+events.on("check_suite:requested", runChecks);
+events.on("check_suite:rerequested", runChecks);
+events.on("check_run:rerequested", runChecks);
 
 // Our main test logic, refactored into a function that returns the job
-function runTests(e, project) {
+function createBuildJob(e, project) {
   // Create a new job
-  var testRunner = new Job("test-runner");
+  var buildJob = new Job("build-job");
 
   // We want our job to run the stock Docker Python 3 image
-  testRunner.image = "jekyll/jekyll";
+  buildJob.image = "jekyll/jekyll";
 
   // Now we want it to run these commands in order:
-  testRunner.tasks = [
+  buildJob.tasks = [
     "cd /src",
     "jekyll build"
   ];
 
   // Display logs from the job Pod
-  testRunner.streamLogs = true;
-
-  return testRunner;
+  buildJob.streamLogs = true;
+  return buildJob;
 }
 
 // This runs our main test job, updating GitHub along the way
-function checkRequested(e, p) {
+async function runChecks(e, p) {
   console.log("check requested");
 
   // This Check Run image handles updating GitHub
@@ -39,7 +38,7 @@ function checkRequested(e, p) {
   const env = {
     CHECK_PAYLOAD: e.payload,
     CHECK_NAME: "Brigade",
-    CHECK_TITLE: "Run Tests",
+    CHECK_TITLE: "Run Test Build",
   };
 
   // For convenience, we'll create three jobs: one for each GitHub Check
@@ -47,7 +46,7 @@ function checkRequested(e, p) {
   const start = new Job("start-run", checkRunImage);
   start.imageForcePull = true;
   start.env = env;
-  start.env.CHECK_SUMMARY = "Beginning test run";
+  start.env.CHECK_SUMMARY = "Beginning test build run";
 
   const end = new Job("end-run", checkRunImage);
   end.imageForcePull = true;
@@ -55,22 +54,24 @@ function checkRequested(e, p) {
 
   // Now we run the jobs in order:
   // - Notify GitHub of start
-  // - Run the tests
+  // - Run the test build
   // - Notify GitHub of completion
   //
   // On error, we catch the error and notify GitHub of a failure.
-  start.run().then(() => {
-    return runTests(e, p).run()
-  }).then( (result) => {
+
+  const buildJob = createBuildJob();
+  try {
+    await start.run();
+    await buildJob.run();
     end.env.CHECK_CONCLUSION = "success"
     end.env.CHECK_SUMMARY = "Build completed"
     end.env.CHECK_TEXT = result.toString()
-    return end.run()
-  }).catch( (err) => {
+    await end.run()
+  } catch (err) {
     // In this case, we mark the ending failed.
     end.env.CHECK_CONCLUSION = "failure"
     end.env.CHECK_SUMMARY = "Build failed"
     end.env.CHECK_TEXT = `Error: ${ err }`
-    return end.run()
-  });
+    await end.run()
+  }
 }
