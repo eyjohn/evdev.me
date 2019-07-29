ea4db66079cc5afd5a6b135076d8faac16b7103b
Evgeny Yakimov <john.yakimov@gmail.com>
2019-07-29 22:33:26 +0100

    More secure public brigade

--- brigade.js
+++ brigade.js
@@ -1,8 +1,5 @@
 const { events, Job } = require("brigadier");
 
-events.on("check_suite:requested", runChecks);
-events.on("check_suite:rerequested", runChecks);
-events.on("check_run:rerequested", runChecks);
 events.on("push", runBuildAndDeploy);
 
 // Build the job, optionally write built files to `buildDir`
@@ -29,57 +26,57 @@ function createBuildJob(e, p, buildDir) {
   return buildJob;
 }
 
-// This runs our main test job, updating GitHub along the way
-async function runChecks(e, p) {
-  console.log("check requested");
+// // This runs our main test job, updating GitHub along the way
+// async function runChecks(e, p) {
+//   console.log("check requested");
 
-  // This Check Run image handles updating GitHub
-  const checkRunImage = "brigadecore/brigade-github-check-run:latest";
+//   // This Check Run image handles updating GitHub
+//   const checkRunImage = "brigadecore/brigade-github-check-run:latest";
 
-  // Common configuration
-  const env = {
-    CHECK_PAYLOAD: e.payload,
-    CHECK_NAME: "Brigade",
-    CHECK_TITLE: "Run Test Build",
-  };
+//   // Common configuration
+//   const env = {
+//     CHECK_PAYLOAD: e.payload,
+//     CHECK_NAME: "Brigade",
+//     CHECK_TITLE: "Run Test Build",
+//   };
 
-  // For convenience, we'll create three jobs: one for each GitHub Check
-  // stage.
-  const start = new Job("start-run", checkRunImage);
-  start.imageForcePull = true;
-  start.env = env;
-  start.env.CHECK_SUMMARY = "Beginning test build run";
+//   // For convenience, we'll create three jobs: one for each GitHub Check
+//   // stage.
+//   const start = new Job("start-run", checkRunImage);
+//   start.imageForcePull = true;
+//   start.env = env;
+//   start.env.CHECK_SUMMARY = "Beginning test build run";
 
-  const end = new Job("end-run", checkRunImage);
-  end.imageForcePull = true;
-  end.env = env;
+//   const end = new Job("end-run", checkRunImage);
+//   end.imageForcePull = true;
+//   end.env = env;
 
-  // Now we run the jobs in order:
-  // - Notify GitHub of start
-  // - Run the test build
-  // - Notify GitHub of completion
-  //
-  // On error, we catch the error and notify GitHub of a failure.
+//   // Now we run the jobs in order:
+//   // - Notify GitHub of start
+//   // - Run the test build
+//   // - Notify GitHub of completion
+//   //
+//   // On error, we catch the error and notify GitHub of a failure.
 
-  const buildDir = "/mnt/brigade/share/site/";
-  const buildJob = createBuildJob(e, p, buildDir);
-  const deployJob = createDeployJob(e, p, buildDir, true); // Staging deployment
-  try {
-    await start.run();
-    await buildJob.run();
-    await deployJob.run();
-    end.env.CHECK_CONCLUSION = "success";
-    end.env.CHECK_SUMMARY = "Build completed";
-    end.env.CHECK_TEXT = result.toString();
-    await end.run();
-  } catch (err) {
-    // In this case, we mark the ending failed.
-    end.env.CHECK_CONCLUSION = "failure";
-    end.env.CHECK_SUMMARY = "Build failed";
-    end.env.CHECK_TEXT = `Error: ${err}`;
-    await end.run();
-  }
-}
+//   const buildDir = "/mnt/brigade/share/site/";
+//   const buildJob = createBuildJob(e, p, buildDir);
+//   const deployJob = createDeployJob(e, p, buildDir, true); // Staging deployment
+//   try {
+//     await start.run();
+//     await buildJob.run();
+//     await deployJob.run();
+//     end.env.CHECK_CONCLUSION = "success";
+//     end.env.CHECK_SUMMARY = "Build completed";
+//     end.env.CHECK_TEXT = result.toString();
+//     await end.run();
+//   } catch (err) {
+//     // In this case, we mark the ending failed.
+//     end.env.CHECK_CONCLUSION = "failure";
+//     end.env.CHECK_SUMMARY = "Build failed";
+//     end.env.CHECK_TEXT = `Error: ${err}`;
+//     await end.run();
+//   }
+// }
 
 function createDeployJob(e, p, buildDir, staging) {
   const firebaseProject = staging ? p.secrets.FIREBASE_PROJECT_STAGING : p.secrets.FIREBASE_PROJECT_PRODUCTION;
@@ -94,7 +91,17 @@ function createDeployJob(e, p, buildDir, staging) {
 async function runBuildAndDeploy(e, p) {
   const buildDir = "/mnt/brigade/share/site/";
   const buildJob = createBuildJob(e, p, buildDir);
-  const deployJob = createDeployJob(e, p, buildDir);
+
+  var staging;
+  if (e.revision.ref == "refs/heads/master") {
+    staging = false;
+  } else if (e.revision.ref == "refs/heads/staging") {
+    staging = true;
+  } else {
+    return; // Nothing to do
+  }
+
+  const deployJob = createDeployJob(e, p, buildDir, staging);
 
   await buildJob.run();
   await deployJob.run();
